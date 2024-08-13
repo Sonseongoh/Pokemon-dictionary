9478f97cd3e91f842db9ca671c869ebc7270a5d3
Sonseongoh <nobleser@naver.com>
2024-08-13 18:35:19 +0900

    검색기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,65 @@ import PokeCard from "./components/PokeCard";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const url = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
   useEffect(() => {
-    fetchPokeData();
+    fetchPokeData(true);
   }, []);
 
-  const fetchPokeData = async () => {
+  const fetchPokeData = async (isFirstFetch) => {
     try {
+      const offsetValue = isFirstFetch ? 0 : offset + limit;
+      const url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offsetValue}`;
       const response = await axios.get(url);
-      setPokemons(response.data.results);
+      setPokemons([...pokemons, ...response.data.results]);
+      setOffset(offsetValue);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleSearchInput = async (event) => {
+    setSearchTerm(event.target.value);
+    if (event.target.value.length > 0) {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${event.target.value}`
+        );
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+          name: searchTerm,
+        };
+        setPokemons([pokemonData]);
+      } catch (error) {
+        setPokemons([]);
+        console.log(error);
+      }
+    } else {
+      fetchPokeData(true);
     }
   };
 
   return (
     <article className="pt-6 ">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        Input form
+        <div className="relative z-50">
+          <form className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto"></form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchInput}
+            className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
+          />
+          <button
+            type="submit"
+            className="text-xs bg-slate-900 text-slate-300 w-[3rem] h-6 px-1 py-1 rounded-lg text-center ml-1 hover:bg-slate-700"
+          >
+            Search
+          </button>
+        </div>
       </header>
       <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
@@ -38,6 +78,11 @@ function App() {
           )}
         </div>
       </section>
+      <div onClick={() => fetchPokeData(false)} className="text-center">
+        <button className="bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white">
+          Show More
+        </button>
+      </div>
     </article>
   );
 }
